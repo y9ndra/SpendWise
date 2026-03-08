e92f48aba432e48bd2b44638df158aaa3072ea64
Yugi <yugendhra18@gmail.com>
2026-03-08 12:23:59 +0530

    Expense model is added

---
+++ models/expense.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const expenseSchema = new mongoose.Schema({
+    title:{
+        type : String,
+        required : true
+    },
+    amount:{
+        type : Number,
+        required : true
+    },
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+});
+
+const Expense = mongoose.model("Expense", expenseSchema);
+
+module.exports = Expense;
