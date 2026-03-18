b5d656fd15e470d097a470513dbc835a12b19e20
Yugi <yugendhra18@gmail.com>
2026-03-18 22:23:49 +0530

    added delete expense and category routes

--- routes/expense.route.js
+++ routes/expense.route.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const authMiddleware = require("../middlewares/auth.middleware.js");
+const {addExpense,viewExpense,deleteExpense,category} = require("../controller/expense.controller.js");
 
+router.post("/add",authMiddleware,addExpense);
+router.get("/get",authMiddleware,viewExpense);
+router.post("/del/:id",authMiddleware,deleteExpense);
+router.get("/",authMiddleware,category);
 
 module.exports = router;
