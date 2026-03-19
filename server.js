6523fcb880578d2a02e412e579074c238115ed7a
Yugi <yugendhra18@gmail.com>
2026-03-20 00:02:32 +0530

    feat(server): add helmet middleware for enhanced security and update package.json

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const helmet = require("helmet");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth.route.js")
 const expenseRoute = require("./routes/expense.route.js");
 require("dotenv").config();
 
 const app = express();
+app.use(helmet());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
